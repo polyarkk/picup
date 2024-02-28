798c99969d8afcdc5885ab48e862d2cf5671dcd4
brz <1781176460@qq.com>
2024-02-28 23:47:28 +0800

    Update lib.rs

--- picup-lib/src/lib.rs
+++ picup-lib/src/lib.rs
@@ -57,12 +57,34 @@ impl<TData> RestResponse<TData> {
         &self.msg
     }
 
-    pub fn data(&self) -> Option<&TData> {
-        self.data.as_ref()
+    pub fn data(self) -> Option<TData> {
+        self.data
     }
 }
 
-pub fn picup(base_url: &String, token: &String, file_paths: &[String]) -> Result<Vec<String>> {
+/**
+   Uploads images to the PicUp server using `/upload` API.
+
+   `base_url`: Base url of the API. On calling, the url will be: `{baseurl}/picup/upload`.
+
+   `token`: Access token for the API.
+
+   `file_paths`: Paths of image files.
+
+   If OK, the function will return a vector of urls for images uploaded, or error messages otherwise.
+
+   ```rust
+    let url = "http://127.0.0.1:19190";
+    let token = "baka";
+    let file_paths = vec!["/path/to/img1", "/path/to/img2"];
+
+    let result = picup(url, token, &file_paths);
+   ```
+*/
+pub fn picup<TPath>(base_url: &str, token: &str, file_paths: &[TPath]) -> Result<Vec<String>>
+where
+    TPath: AsRef<std::path::Path>,
+{
     let client = Client::new();
 
     let mut form = Form::new();
@@ -84,5 +106,5 @@ pub fn picup(base_url: &String, token: &String, file_paths: &[String]) -> Result
         return Err(Error::from(res.msg().as_str()));
     }
 
-    return Ok(res.data().unwrap().to_vec());
+    return Ok(res.data().take().unwrap());
 }
